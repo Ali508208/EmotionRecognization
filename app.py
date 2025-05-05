ec35c18ee1e9aa613e1d15a27dfdb5875ead7694
Ali Hamza <79903485+Ali508208@users.noreply.github.com>
2025-05-05 16:40:02 +0500

    update the bugs

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def predict_emotion():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+   
+    app.run(debug=True,host='0.0.0.0', port=5000, use_reloader=False)
